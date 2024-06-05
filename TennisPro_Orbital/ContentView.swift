3e0a523212de7cd188fd752bcd60bdcaf8a8c2dc
caroline1233456 <caroline-qingru.yang@outlook.com>
2024-06-05 11:09:31 +0800

    fourth commit, with welcome page added to .xcodeproj file

--- TennisPro_Orbital/ContentView.swift
+++ TennisPro_Orbital/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
     @State private var email:String=""
     @State private var password:String=""
     var body: some View {
-       AuthView()
+       WelcomeView()
     }
 }
  
--- TennisPro_Orbital/views/WelcomeView.swift
+++ TennisPro_Orbital/views/WelcomeView.swift
@@ -2,17 +2,51 @@
 //  WelcomeView.swift
 //  TennisPro_Orbital
 //
-//  Created by Yuexi Song on 4/6/24.
+//  Created by 杨清如 on 5/6/24.
 //
 
 import SwiftUI
 
 struct WelcomeView: View {
+    @State private var showAuthView = false
+    @State var degreesRotating = 0.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+                VStack(alignment: .center){
+                    Group{
+                        Text("Welcome to TennisPro!").font(.system(size: 45, weight: .light, design: .serif))
+                            .italic()
+                            .frame(alignment:.center)
+                        Image("tennis")
+                            .resizable()
+                            .scaledToFit()
+                            .frame( width:150, height: 100)
+                            .rotationEffect(.degrees(degreesRotating))
+                            .onAppear {
+                                withAnimation(.easeInOut(duration: 1)
+                                    .speed(0.1).repeatForever(autoreverses: false)) {
+                                        degreesRotating = 360.0
+                                    }
+                            }
+                        
+                        Button(action: {
+                            showAuthView = true
+                        }, label: {
+                            Text("Continue")
+                                .foregroundColor(.blue)
+                                .frame(width: 200, height: 40)
+                                .background(Color.white)
+                                .cornerRadius(15)
+                                .padding()
+                        })
+                    }.frame(maxHeight: .infinity, alignment: .bottom)
+                }
+                .navigationDestination(isPresented: $showAuthView) {
+                    AuthView()
+                }
+            }
     }
 }
 
-#Preview {
-    WelcomeView()
-}
+
