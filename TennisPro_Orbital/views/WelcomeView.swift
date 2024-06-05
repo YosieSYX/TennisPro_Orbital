2482d22d9f7e0015e519f3ef9e7144ad61266778
YosieSYX <E1300499@U.NUS.EDU>
2024-06-04 21:50:25 -0400

    upload welcome page again

---
+++ TennisPro_Orbital/views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  TennisPro_Orbital
+//
+//  Created by Yuexi Song on 4/6/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
