07ce0008ad3aa098fd89e90dc47cd970d167017e
YosieSYX <E1300499@U.NUS.EDU>
2024-06-26 22:20:50 -0400

    UIdesign

---
+++ TennisPro_Orbital/views/AnalysisView.swift
@@ -0,0 +1,18 @@
+//
+//  AnalysisView.swift
+//  TennisPro_Orbital
+//
+//  Created by Yuexi Song on 26/6/24.
+//
+
+import SwiftUI
+
+struct AnalysisView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnalysisView()
+}
